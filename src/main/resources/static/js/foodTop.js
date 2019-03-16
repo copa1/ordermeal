c10a4adf3f26e70910ab0c01ad81f17931db6130
copa_glb <copa_glb@copa.com>
2019-03-16 21:29:28 +0800

    day27-3 v1.01

--- src/main/resources/static/js/foodTop.js
+++ src/main/resources/static/js/foodTop.js
@@ -6,6 +6,15 @@ $(function () {
        async: false,//同步加载（必须加）
        success:function (result) {
            // console.log(result);
+           if (result.extend.top==""){
+               $("#rankTable").empty();
+               var div1=$(' <div class="row">\n' +
+                   '        <div class="col-md-12"><h2>售卖菜品总排行榜</h2></div>\n' +
+                   '    </div>');
+               var div2=$('<div class="row"><div class="col-md-12 text-center"><img src="/img/sendFoodTopEmpty.jpg"></div></div>');
+               $("#rankTable").append(div1).append(div2);
+           }
+           else {
            $.each(result.extend.top,function (index,item) {
                var rank=$('<td>#'+(index+1)+'</td>');
                var foodName=$('<td>'+item.food.name+'</td>');
@@ -17,8 +26,9 @@ $(function () {
                    .append(foodNum)
                    .appendTo("tbody");
            });
-       }
+       }}
    })
+
 });
 
 $("#topBackButton").click(function () {
